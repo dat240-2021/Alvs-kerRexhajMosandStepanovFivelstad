0a0f52111dffccdfb4f56ea517d4410b2cccfee4
Arkadiy Stepanov <arkadiy93@hotmail.com>
2021-10-29 16:06:50 +0200

    add submit event to registration form

--- website/frontend/src/views/auth/Registration.vue
+++ website/frontend/src/views/auth/Registration.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="d-flex vh-100 justify-content-center align-items-center">
-    <form>
+    <form @submit="handleSubmit">
       <div class="form-group mb-2">
         <label for="loginInputUsername">Username</label>
         <input v-model="form.userName" type="text" class="form-control" id="loginInputUsername" placeholder="Enter username">
@@ -15,7 +15,7 @@
       </div>
       <div class="form-group d-flex  justify-content-around">
         <router-link type="submit" class="btn btn-secondary" to="/">Go back</router-link>
-        <button type="submit" class="btn btn-primary" :disabled="submitting">Login</button>
+        <button type="submit" class="btn btn-primary" :disabled="submitting">Register</button>
       </div>
     </form>
   </div>
@@ -46,7 +46,6 @@ export default {
     handleSubmit(e) {
       e.preventDefault();
       this.submitting = true;
-
       const { userName, password } = this.form;
       auth.registrateUser(userName, password)
         .then(setAuthUser)
