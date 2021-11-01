b8fa95d3dfd2f642d9deedb5be77ccc6e876f986
Arkadiy Stepanov <arkadiy93@hotmail.com>
2021-11-01 20:18:44 +0100

    add initial containers

--- website/frontend/src/views/Home.vue
+++ website/frontend/src/views/Home.vue
@@ -1,6 +1,112 @@
 <template>
-  <div class="home">
-    Home
+  <div class="container vh-100 py-5">
+    <div class="row mt-5 h-25 justify-content-center">
+      <div class="col-8 d-flex">
+        <div class="leaderboard border border-secondary flex-grow-1">
+          Leaderboard
+        </div>
+      </div>
+    </div>
+    <div class="row mt-3 justify-content-center">
+      <div class="col d-flex justify-content-center">
+        <router-link class="btn btn-primary" to="/game">
+          Create game
+        </router-link>
+      </div>
+    </div>
+    <div class="row h-50 mt-5 justify-content-center">
+      <div class="col text-center d-flex flex-column h-100">
+        <h4>Available games</h4>
+        <div
+          class="
+            games-list
+            border border-secondary
+            d-flex
+            h-100
+            overflow-scroll
+          "
+        >
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Game Type</th>
+                <th scope="col">Capacity</th>
+                <th scope="col">Join</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td><button class="btn">Join</button></td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td><button class="btn">Join</button></td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Larry the Bird</td>
+                <td>@twitter</td>
+                <td><button class="btn">Join</button></td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Larry the Bird</td>
+                <td>@twitter</td>
+                <td><button class="btn">Join</button></td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Larry the Bird</td>
+                <td>@twitter</td>
+                <td><button class="btn">Join</button></td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Larry the Bird</td>
+                <td>@twitter</td>
+                <td><button class="btn">Join</button></td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Larry the Bird</td>
+                <td>@twitter</td>
+                <td><button class="btn">Join</button></td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Larry the Bird</td>
+                <td>@twitter</td>
+                <td><button class="btn">Join</button></td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Larry the Bird</td>
+                <td>@twitter</td>
+                <td><button class="btn">Join</button></td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Larry the Bird</td>
+                <td>@twitter</td>
+                <td><button class="btn">Join</button></td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Larry the Bird</td>
+                <td>@twitter</td>
+                <td><button class="btn">Join</button></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,3 +117,9 @@ export default defineComponent({
   name: "Home",
 });
 </script>
+
+<style scoped>
+.leaderboard, .games-list {
+  background: #e7efbd;
+}
+</style>
