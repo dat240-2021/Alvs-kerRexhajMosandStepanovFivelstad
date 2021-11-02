4b19e4b4adbec1783dfa676087b38fc7ba73f9ce
ardijanr <a.rexhaj@stud.uis.no>
2021-11-02 13:34:58 +0100

    added pipelines to auth context

--- website/backend/Controllers/Authentication/LoginController.cs
+++ website/backend/Controllers/Authentication/LoginController.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Threading.Tasks;
 using Controllers.Authentication.Models;
+using Domain.Authentication.Pipelines;
 using Domain.Authentication.Services;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 // using Microsoft.AspNetCore.Http.StatusCode;
 using Microsoft.Extensions.Logging;
@@ -15,18 +17,18 @@ namespace Controllers.Authentication
     public class LoginController : ControllerBase
     {
         private readonly ILogger<LoginController> _logger;
-        private readonly IAuthenticationService _authServ;
+		private readonly IMediator _mediator;
 
-        public LoginController(ILogger<LoginController> logger, IAuthenticationService authServ)
+        public LoginController(ILogger<LoginController> logger, IMediator mediator)
         {
             _logger = logger;
-            _authServ = authServ;
+            _mediator = mediator;
         }
 
         [HttpPost]
         public async Task<IActionResult> Post(UserRequestDto user){
 
-            if (await _authServ.LoginUser(user.Username,user.Password)){
+            if ( (await _mediator.Send(new LoginUser.Request(user.Username,user.Password))).Success ){
                 return Ok(new UserResponseDto{Username = user.Username});
             }
 
--- website/backend/Controllers/Authentication/RegistrationController.cs
+++ website/backend/Controllers/Authentication/RegistrationController.cs
@@ -5,6 +5,8 @@ using Domain.Authentication.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Controllers.Authentication.Models;
+using MediatR;
+using Domain.Authentication.Pipelines;
 
 namespace Controllers.Authentication
 {
@@ -13,22 +15,24 @@ namespace Controllers.Authentication
     public class RegistrationController : ControllerBase
     {
         private readonly ILogger<RegistrationController> _logger;
-        private readonly IAuthenticationService _authServ;
+        private readonly IMediator _mediator;
 
-        public RegistrationController(ILogger<RegistrationController> logger, IAuthenticationService authServ)
+        public RegistrationController(ILogger<RegistrationController> logger, IMediator mediator)
         {
             _logger = logger;
-            _authServ = authServ;
+            _mediator = mediator;
 
         }
 
         [HttpPost]
         public async Task<IActionResult> Post(UserRequestDto user){
-            if (await _authServ.RegisterUser(user.Username,user.Password)){
+            var result = await _mediator.Send(new RegisterUser.Request(user.Username,user.Password));
+
+            if (result.Success){
                 return Ok(new UserResponseDto{Username = user.Username});
             }
 
-            return UnprocessableEntity();
+            return UnprocessableEntity(result.errors);
         }
     }
 
