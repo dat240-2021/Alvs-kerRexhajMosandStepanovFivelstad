1cdef75c8c6840ad3b10f546ce7fe15c2fac69e0
Fivel1974 <t.fivelstad@stud.uis.no>
2021-11-07 10:18:26 +0100

    Fix issue with Get Image List by ID pipeline

--- website/backend/Core/Domain/Image/Pipelines/GetImageIdListByCategory.cs
+++ website/backend/Core/Domain/Image/Pipelines/GetImageIdListByCategory.cs
@@ -11,7 +11,7 @@ namespace backend.Core.Domain.Image.Pipelines
 {
 	public class GetImageIdListByCategory
 	{
-		//finish .....
+	
 		public record Request(List<string> Categories, Guid? UserId) : IRequest<List<int>>;
 
 		public class Handler : IRequestHandler<Request, List<int>>
@@ -23,16 +23,20 @@ namespace backend.Core.Domain.Image.Pipelines
 			public async Task<List<int>> Handle(Request request, CancellationToken cancellationToken)
 			{
 				var categoryImageIdList = new List<int>();
-				if (request.UserId != null)
-				{
-					var userList = await _db.Image.Include(ic => ic.Category.Category).Where(i => i.Category.Category == "My Images").Where(i => i.UserId == request.UserId).Select(i => i.Id).ToListAsync(cancellationToken);
-					categoryImageIdList.AddRange(userList);
-				}
 
 				foreach (var category in request.Categories)
 				{
-					var tempList = await _db.Image.Include(ic => ic.Category.Category).Where(i => i.Category.Category == category).Select(i => i.Id).ToListAsync(cancellationToken);
-					categoryImageIdList.AddRange(tempList);
+					if (request.UserId != null && category == "My Images")
+					{
+						var userList = await _db.Image.Include(ic => ic.Category.Category).Where(i => i.Category.Category == "My Images").Where(i => i.UserId == request.UserId).Select(i => i.Id).ToListAsync(cancellationToken);
+						categoryImageIdList.AddRange(userList);
+					}
+
+					if (category != "My Images")
+					{
+						var tempList = await _db.Image.Include(ic => ic.Category.Category).Where(i => i.Category.Category == category).Select(i => i.Id).ToListAsync(cancellationToken);
+						categoryImageIdList.AddRange(tempList);
+					}
 				}
 				return categoryImageIdList;
 			}
