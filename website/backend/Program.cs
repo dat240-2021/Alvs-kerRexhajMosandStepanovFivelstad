9948c997d0dcf5ebb3c2d00a382ff0e759057663
Ardijan Rexhaj <56295664+ardijanr@users.noreply.github.com>
2021-11-08 11:18:42 +0100

    Revert "Update og Image Context and Image Import Script"

--- website/backend/Program.cs
+++ website/backend/Program.cs
@@ -1,11 +1,8 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Data.Sqlite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -16,7 +13,6 @@ namespace backend
     {
         public static void Main(string[] args)
         {
-            ImportDefaultImages();
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -26,34 +22,5 @@ namespace backend
                 {
                     webBuilder.UseStartup<Startup>();
                 });
-        
-        private static void ImportDefaultImages()
-        {
-            var db = new SqliteConnection($"Data Source={Path.Combine("Infrastructure", "Data", "game.db")}");
-            db.Open();
-            var query = db.CreateCommand();
-            query.CommandText = "SELECT * FROM Image";
-            var result = query.ExecuteReader();
-            bool runImport;
-            if (result.HasRows == false)
-            {
-                runImport = true;
-            }
-            else
-            {
-                runImport = false; }
-            db.Close();
-            
-                
-            if (runImport)
-            {
-                Process proc = new Process();
-                proc.StartInfo.FileName = "Python.exe";
-                proc.StartInfo.UseShellExecute = false;
-                proc.StartInfo.Arguments = ".\\Infrastructure\\Data\\Insert_Images_to_Db.py";
-                proc.Start();
-                Console.WriteLine("Python Script is running for importing image data to database tables");
-            }
-        }
     }
 }
