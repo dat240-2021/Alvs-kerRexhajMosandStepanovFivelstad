ce3cbaaa8a88673798f0b3dd85515f1026a33d29
Sindre <sindreum@gmail.com>
2021-11-09 21:43:53 +0100

    type in uploadimage in url to get to this page. I used bootstrap to make boxes devided in pares, one for the image and one for image description. The image box is empty for now

--- website/frontend/src/views/UploadImages.vue
+++ website/frontend/src/views/UploadImages.vue
@@ -1,44 +1,67 @@
 <template>
     <div class="container vh-100 py-5">
         
-        <div class="col d-flex justify-content-end">
+        <div class="col">
             <router-link class="btn btn-primary" to="/home">
                 Return home
             </router-link>
         </div>
-        <div class="col d-flex justify-content-center">
+        <div class="col">
             <h1>Upload Image</h1>
+        </div>
+        <div class="row" >
+            <div class="row d-flex justify-content-start" v-for="i in 10" :key="i">
+                <div class="col border border-success" >
+                    image {{i}} goes here
+                    
+                </div>
+                <div class="col border border-success">
+                       <table>
+                            <tr>
+                                <th>Image name</th>
+                            </tr>
+                            <tr>
+                                <td>Image title (what to guess)</td>
+                            </tr>
+                            <tr>
+                                <td>Category</td>
+                            </tr>
+                        </table>
+                    </div>
+                    
+                
+            </div>
         </div>         
     </div>
 </template>
 
 <script lang="ts">
-import Input from "@/components/Form/Input.vue";
-import Submit from "@/components/Form/Submit.vue";
+//import Input from "@/components/Form/Input.vue";
+//import Submit from "@/components/Form/Submit.vue";
 
 
-declare interface BaseComponentData {
-
-}
+//declare interface BaseComponentData {
+//
+//}
 
 export default {
     name: "UploadImages",
     components: {
-        Input,
-        Submit,
+        //Input,
+        //Submit,
     },
     
-    data() : BaseComponentData {
-        return {
-        };
-    },
-    methods:{      
-    }
+    //data() : BaseComponentData {
+    //    return {
+    //    };
+    //},
+    //methods:{      
+    //}
 };
 </script>
 
 <style scoped>
-.row {
+.col {
     margin: 1em;
 }
 </style>
