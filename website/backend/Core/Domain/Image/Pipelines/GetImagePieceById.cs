f6bb877263d09c1d6ad56ff5aab4624f39657d6c
ardijanr <a.rexhaj@stud.uis.no>
2021-11-09 21:47:37 +0100

    changed GetImageById pipeline

--- website/backend/Core/Domain/Image/Pipelines/GetImagePieceById.cs
+++ website/backend/Core/Domain/Image/Pipelines/GetImagePieceById.cs
@@ -10,29 +10,27 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Domain.Image.Pipelines
 {
-	public class GetImagePieceById
-	{
-		public record Request(int Id, int SequenceNumber) : IRequest<byte[]>;
+    public class GetImagePieceById
+    {
+        public record Request(int Id, int SequenceNumber) : IRequest<byte[]>;
 
-		public class Handler : IRequestHandler<Request, byte[]>
-		{
-			private readonly GameContext _db;
+        public class Handler : IRequestHandler<Request, byte[]>
+        {
+            private readonly GameContext _db;
 
-			public Handler(GameContext db) => _db = db ?? throw new ArgumentNullException(nameof(db));
+            public Handler(GameContext db) => _db = db ?? throw new ArgumentNullException(nameof(db));
 
-			public async Task<byte[]> Handle(Request request, CancellationToken cancellationToken)
-			{
-				var image = await _db.Images.Include(i => i.Slices).Where(i => i.Id == request.Id).SingleOrDefaultAsync();
-				byte[] imagePiece = null;
-				foreach (var piece in image.Slices)
-				{
-					if (piece.SequenceNumber == request.SequenceNumber)
-					{
-						imagePiece = piece.ImageData;
-					}
-				}
-				return imagePiece;
-			}
-		}
-	}
+            public async Task<byte[]> Handle(Request request, CancellationToken cancellationToken)
+            {
+                var image = await _db.Images.Include(i => i.Slices).Where(i => i.Id == request.Id).SingleOrDefaultAsync();
+                if (image!=null){
+
+                return image.Slices.Find(i => i.SequenceNumber == request.SequenceNumber).ImageData;
+
+                }
+
+                return null;
+            }
+        }
+    }
 }
