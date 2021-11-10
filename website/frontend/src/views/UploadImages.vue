8abcf3a0761f53afdaca8901c6cc0bfdc8a17e6c
Sindre <sindreum@gmail.com>
2021-11-10 14:42:20 +0100

    changed the look of the entire page. Created a table which is ment to show how it will look with imported images. Added a Browse field for uploading images and a button that will upload selected images.

--- website/frontend/src/views/UploadImages.vue
+++ website/frontend/src/views/UploadImages.vue
@@ -1,42 +1,67 @@
 <template>
     <div class="container vh-100 py-5">
-        
-        <div class="col">
-            <router-link class="btn btn-primary" to="/home">
-                Return home
-            </router-link>
+        <div class="row">
+            <div class="col d-flex justify-content-around">
+                <router-link class="btn btn-primary" to="/home">
+                    Return home
+                </router-link>
+                <Submit>Start game</Submit>    
+            </div>
         </div>
-        <div class="col">
+        
+        <div class="col d-flex justify-content-center">
             <h1>Upload Image</h1>
         </div>
-        <div class="row" >
-            <div class="row d-flex justify-content-start" v-for="i in 10" :key="i"><!--this loop must be changed to iterate over images-->
-                <div class="col border border-success" >
-                    image {{i}} goes here
-                    
-                </div>
-                    <div class="col border border-success">
-                        <table>
-                             <tr>
-                                 <th>Image name</th>
-                             </tr>
-                             <tr>
-                                 <td>Image title (what to guess)</td>
-                             </tr>
-                             <tr>
-                                 <td>Category</td>
-                             </tr>
-                        </table>
-                    </div>
-            </div>
+        <div class="row ">
+            <form>
+            <div class="col d-flex justify-content-around">              
+                <p>Upload Images</p>
+                    <Input type="file" @change="onImageSelected"></Input>
+                    <Submit @click="onUploadImage" > Upload</Submit>
+                <p>{{numberOfImages}} files selected</p>
+            </div>   
+            </form>         
+        </div>
+        <form>
+        <div class="table">
+            <table>
+                <tr>
+                    <th>#</th>              
+                    <th>Filename</th>
+                    <th>Title</th>
+                    <th>Category</th>
+                </tr>       
+                <tr v-for="i in 10" :key="i">
+                    <td>{{i}}</td>                
+                    <td>imagename.jpg</td>                
+                    <td>
+                       <Input  
+                           v-model="label"
+                           error=""
+                           type="text"
+                           model-value="Image Title"
+                           id="imagetitle"                               
+                       />
+                    </td>              
+                    <td>
+                       <Input                                                       
+                           error=""
+                           type="text"
+                           model-value="Category"
+                           id="category"                               
+                       />
+                    </td>
+                </tr>         
+            </table>
+        </div>
+        </form>      
         </div>         
-    </div>
 </template>
 
 <script lang="ts">
-// There is no need for input and submit here but, keep it for now.
-//import Input from "@/components/Form/Input.vue";
-//import Submit from "@/components/Form/Submit.vue";
+import Input from "@/components/Form/Input.vue";
+import Submit from "@/components/Form/Submit.vue";
+
 
 export class Image {
     Image: string; //this is the image.
@@ -57,22 +82,36 @@ export class Image {
 
 declare interface BaseComponentData {
     images : Image[],
+    selectedImage: null,
+    label : string,
+    numberOfImages : number,
 }
 
 export default {
     name: "UploadImages",
     components: {
-        //Input,
-        //Submit,
+        Input,
+        Submit,
     },
     
     data() : BaseComponentData {
         return {
-            images: [],
+            images: [] as Image[],
+            selectedImage: null,
+            label: "",
+            numberOfImages: 0,
         };
     },
-    //methods:{      
-    //}
+    methods:{
+        onImageSelected(event: any) {
+            console.log(event)
+            //this.images.Image = event.target.files[0]
+            //this.selectedImage = event.target.files[0]
+        },
+        onUploadImage(){
+            // This is where we send the images to database.
+        }
+    }
 };
 </script>
 
