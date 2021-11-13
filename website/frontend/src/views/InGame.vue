56f3eaa895791f2c2ee613117b322587beea6683
ardijanr <a.rexhaj@stud.uis.no>
2021-11-14 00:11:53 +0100

    slice identification through canvas elemets optimized to a degree, role dependant look -> guess field is hidden for proposer

--- website/frontend/src/views/InGame.vue
+++ website/frontend/src/views/InGame.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="d-flex">
     <div class="ms-auto m-2">
-      <button class="btn btn-outline-primary" type="button" @click="LeaveGame">
+      <button
+        class="btn btn-outline-primary mb-5"
+        type="button"
+        @click="LeaveGame"
+      >
         Leave Game
       </button>
     </div>
   </div>
   <div class="container-fluid min-vh-100">
-    <div class="row">
-      <div>
+    <div class="row mt-5">
+      <div v-if="!isProposer">
         <form @submit.prevent="SendGuess" class="form-control border-0">
-          <div class="input-group mb-3 mt-5">
+          <div class="input-group mb-3">
             <input
               type="text"
               class="form-control"
@@ -20,12 +24,13 @@
               aria-describedby="basic-addon1"
             />
             <div class="input-group-prepend">
-              <button class="btn btn-outline-primary" type="submit">Guess</button>
+              <button class="btn btn-outline-primary" type="submit">
+                Guess
+              </button>
             </div>
           </div>
         </form>
       </div>
-
 
       <div class="col-1">
         <table>
@@ -41,14 +46,18 @@
           </tbody>
         </table>
       </div>
-      <div class="col position-relative" id="canvas-div" :click="AddImage">
-
-            <!-- <canvas id="image-canvas" width="1000" height="1000"></canvas> -->
-            <!-- <img  v-for="i in GenerateImagePaths()" @mousemove="MouseMove" @click="SelectedTile(i)" :key="i.id" class="" style="width:100%;"
-            :src="i.src"
-            /> -->
-
-
+      <div class="col position-relative" id="canvas-div">
+        <div v-if="!isProposer" class="position-relative">
+          <img
+            v-for="im in GenerateImagePaths()"
+            :key="im.id"
+            :src="im.src"
+            style="width: 100%"
+            class="position-absolute top-0 start-0"
+          />
+        </div>
+        <h2 v-else>Solution Text</h2>
+        <!-- <canvas id="image-canvas" width="1000" height="1000"></canvas> -->
       </div>
       <div class="col-2">
         <table>
@@ -97,7 +106,7 @@ export class Slice {
   src: string;
   id: string;
 
-  constructor(src: string,id: string) {
+  constructor(src: string, id: string) {
     this.src = src;
     this.id = "slice-".concat(id);
   }
@@ -109,17 +118,18 @@ declare interface BaseComponentData {
   imageSlices: string[];
   newGuess: string;
   correct: string;
+  isProposer: boolean;
   coords: {
-    x : number,
-    y : number,
-  }
+    x: number;
+    y: number;
+  };
 }
 
 export default defineComponent({
   name: "InGame",
   data(): BaseComponentData {
     return {
-      coords : {x: 0, y : 0},
+      coords: { x: 0, y: 0 },
       players: [
         new Player("Jamie Lannister", 10, "1"),
         new Player("The Hound", 11, "2"),
@@ -136,6 +146,8 @@ export default defineComponent({
         new Guess("Cat"),
       ] as Guess[],
 
+      isProposer: true,
+
       imageSlices: [],
       //incorrect: true,
       correct: "Fish",
@@ -144,129 +156,175 @@ export default defineComponent({
     };
   },
   methods: {
-    mounted(){
+    mounted() {
       console.log("FUCK OFF");
     },
     SendGuess() {
       console.log(this.newGuess);
-      this.AddImages();
-
     },
-
-    LeaveGame(){
+    LeaveGame() {
       console.log("leaving game");
+      this.AddImageProposer();
     },
 
-    AddImages() {
+    AddImageProposer() {
+      //identifies role
+      this.isProposer = true;
+
       var paths = this.GenerateImagePaths();
-      var div = document.getElementById('canvas-div')
-      if (div==null) { console.log("NOT FOUND: canvas"); return; };
-      var height = div.clientHeight;
-      var width = div.clientHeight;
-      console.log(height);
-      console.log(width);
-      for (var i=0; i<paths.length ; i++){
-      this.AddImage(paths[i].src,800);
+      var div = document.getElementById("canvas-div");
+      if (div == null) {
+        return;
       }
-    },
-    AddImage(path:string,inheight: number){
-      var div = document.getElementById('canvas-div')
-      if (div==null) { console.log("NOT FOUND: canvas"); return; };
-      var canvas = document.createElement('canvas');
 
-      canvas?.classList.add("position-absolute","top-0","start-0");
+      //scales the image to a max size
+      //optimises draw time for canvas
+      const width = 500;
+      for (var i = 0; i < paths.length; i++) {
+        this.AddSliceProposer(paths[i].src, width);
+      }
+
+      div.style.transformOrigin = "0 0";
+      console.log(div.clientWidth);
+      //use css to scale, much faster.
+      div.style.transform = "scale(" + div.clientWidth / width + ")";
+    },
+
+    AddSliceProposer(path: string, inWidth: number) {
+      var div = document.getElementById("canvas-div");
+      var canvas = document.createElement("canvas");
+
+      canvas?.classList.add("position-absolute", "top-0", "start-0");
+      canvas.style.filter = "brightness(60%)";
+
+      canvas?.addEventListener("click", this.SelectedTile);
+
       div?.appendChild(canvas);
-
-      var context = canvas.getContext('2d');
-      if (context==null) { console.log("NOT FOUND: context"); return; };
-
-
-        var img = new Image();
-        img.src = path;
-
-        img.onload = function () {
-          console.log("FUCK OFF")
-            //scale the image and canvas
-            canvas.height = inheight;
-            canvas.width = img.width * inheight/img.width;
-            img.width = canvas.width;
-            img.height = canvas.height;
-          // if (context==null) { console.log("NOT FOUND: context 2"); return; };
-          context?.drawImage(img, 0, 0, img.width,img.height);
+      var context = canvas.getContext("2d");
+      if (context == null) {
+        console.log("NOT FOUND: context");
+        return;
       }
 
+      var img = new Image();
+      img.src = path;
+      img.crossOrigin = "anonymous";
 
+      img.onload = function () {
+        //scale the image and canvas
+        canvas.height = (img.height * inWidth) / img.height;
+        canvas.width = inWidth;
+        img.width = canvas.width;
+        img.height = canvas.height;
+
+        context?.drawImage(
+          img,
+          0,
+          0,
+          Math.floor(img.width),
+          Math.floor(img.height)
+        );
+      };
     },
-    SelectedTile(i : any){
-      // for (var i in this.GenerateImagePaths()){
-      console.log("wdhadwadwa")
-    },
-    MouseMove(event : any){
-      var rect = event.target.getBoundingClientRect();
-      this.coords.x = event.clientX - rect.left; //x position within the element.
-      this.coords.y = event.clientY - rect.top; //
-    },
+    SelectedTile(event: any) {
+      var x = event.offsetX;
+      var y = event.offsetY;
+      var children = document.getElementById("canvas-div")?.children;
+      if (children == null) {
+        return;
+      }
 
-    GenerateImagePaths() {
- var _paths = ["https://i.ibb.co/rkvRk2v/1.png",
-"https://i.ibb.co/Qn2fzTD/1.png",
-"https://i.ibb.co/1vv6ZHx/1.png",
-"https://i.ibb.co/5cRWN0W/1.png",
-"https://i.ibb.co/Lrj1JqZ/1.png",
-"https://i.ibb.co/1rnGKwS/1.png",
-"https://i.ibb.co/T2KbjFv/1.png",
-"https://i.ibb.co/CM0yLDb/1.png",
-"https://i.ibb.co/X8XF0Zm/1.png",
-"https://i.ibb.co/tcLrjyX/1.png",
-"https://i.ibb.co/n79x3d2/1.png",
-"https://i.ibb.co/T2sHsZw/1.png",
-"https://i.ibb.co/9VcfNNW/1.png",
-"https://i.ibb.co/RN7xQCP/1.png",
-"https://i.ibb.co/6mFbwT3/1.png",
-"https://i.ibb.co/Db0hrxN/1.png",
-"https://i.ibb.co/0C3nvXF/1.png",
-"https://i.ibb.co/HFHsB8L/1.png",
-"https://i.ibb.co/RQjKCtK/1.png",
-"https://i.ibb.co/7XzdmMb/1.png",
-"https://i.ibb.co/LRr44qp/1.png",
-"https://i.ibb.co/brqc1pT/1.png",
-"https://i.ibb.co/d6V6VBL/1.png",
-"https://i.ibb.co/W2ZWKmD/1.png",
-"https://i.ibb.co/PFpBRp7/1.png",
-"https://i.ibb.co/KsV0xfX/1.png",
-"https://i.ibb.co/VSBHvFV/1.png",
-"https://i.ibb.co/TKC1Vm2/1.png",
-"https://i.ibb.co/Mg0KYv5/1.png",
-"https://i.ibb.co/9t2zyZr/1.png",
-"https://i.ibb.co/2tntL8b/1.png",
-"https://i.ibb.co/V9CLzkf/1.png",
-"https://i.ibb.co/kSm7nXY/1.png",
-"https://i.ibb.co/VmZGRkW/1.png",
-"https://i.ibb.co/CWg1TjL/1.png",
-"https://i.ibb.co/KNfp9db/1.png",
-"https://i.ibb.co/qDHvLtZ/1.png",
-"https://i.ibb.co/0y8ndtv/1.png",
-"https://i.ibb.co/P4Ktddb/1.png",
-"https://i.ibb.co/Z8MsfvQ/1.png",
-"https://i.ibb.co/myRTRX7/1.png",
-"https://i.ibb.co/sgxszkf/1.png",
-"https://i.ibb.co/jkqdKhQ/1.png",
-"https://i.ibb.co/kMpmyGN/1.png",
-"https://i.ibb.co/4W66fw0/1.png",
-"https://i.ibb.co/RN4FvdH/1.png",
-"https://i.ibb.co/N3TKrNx/1.png",
-"https://i.ibb.co/xGmGVrV/1.png",
-"https://i.ibb.co/B6VWy5f/1.png"];
-
-        let paths : Slice[] = [];
-
-        for (let i = 0; i<_paths.length; i++){
-          paths.push(new Slice(_paths[i],i as any as string));
+      for (let i = 0; i < children.length; i++) {
+        var cnvs = children.item(i);
+        if (!(cnvs instanceof HTMLCanvasElement)) {
+          return;
+        }
+        var canvas: HTMLCanvasElement = cnvs;
+        var context = canvas.getContext("2d");
+        if (context == null) {
+          return;
         }
 
-        return paths
-    }
+        var data = context.getImageData(0, 0, canvas.width, canvas.height);
+        let index = (y * data.width + x) * 4;
+        let alpha = data?.data[index + 3];
+        if (alpha > 5) {
+          console.log(i);
+          canvas.style.filter = "brightness(100%)";
+          //here we call the api to send the image slice id.
+          return;
+        }
+      }
+    },
+
+    // HandleSliceGuesser(){},
+    // HandleImageProposer(){},
+    // Handle
+
+    GenerateImagePaths() {
+      var _paths = [
+        "https://i.ibb.co/rkvRk2v/1.png",
+        "https://i.ibb.co/Qn2fzTD/1.png",
+        "https://i.ibb.co/1vv6ZHx/1.png",
+        "https://i.ibb.co/5cRWN0W/1.png",
+        "https://i.ibb.co/Lrj1JqZ/1.png",
+        "https://i.ibb.co/1rnGKwS/1.png",
+        "https://i.ibb.co/T2KbjFv/1.png",
+        "https://i.ibb.co/CM0yLDb/1.png",
+        "https://i.ibb.co/X8XF0Zm/1.png",
+        "https://i.ibb.co/tcLrjyX/1.png",
+        "https://i.ibb.co/n79x3d2/1.png",
+        "https://i.ibb.co/T2sHsZw/1.png",
+        "https://i.ibb.co/9VcfNNW/1.png",
+        "https://i.ibb.co/RN7xQCP/1.png",
+        "https://i.ibb.co/6mFbwT3/1.png",
+        "https://i.ibb.co/Db0hrxN/1.png",
+        "https://i.ibb.co/0C3nvXF/1.png",
+        "https://i.ibb.co/HFHsB8L/1.png",
+        "https://i.ibb.co/RQjKCtK/1.png",
+        "https://i.ibb.co/7XzdmMb/1.png",
+        "https://i.ibb.co/LRr44qp/1.png",
+        "https://i.ibb.co/brqc1pT/1.png",
+        "https://i.ibb.co/d6V6VBL/1.png",
+        "https://i.ibb.co/W2ZWKmD/1.png",
+        "https://i.ibb.co/PFpBRp7/1.png",
+        "https://i.ibb.co/KsV0xfX/1.png",
+        "https://i.ibb.co/VSBHvFV/1.png",
+        "https://i.ibb.co/TKC1Vm2/1.png",
+        "https://i.ibb.co/Mg0KYv5/1.png",
+        "https://i.ibb.co/9t2zyZr/1.png",
+        "https://i.ibb.co/2tntL8b/1.png",
+        "https://i.ibb.co/V9CLzkf/1.png",
+        "https://i.ibb.co/kSm7nXY/1.png",
+        "https://i.ibb.co/VmZGRkW/1.png",
+        "https://i.ibb.co/CWg1TjL/1.png",
+        "https://i.ibb.co/KNfp9db/1.png",
+        "https://i.ibb.co/qDHvLtZ/1.png",
+        "https://i.ibb.co/0y8ndtv/1.png",
+        "https://i.ibb.co/P4Ktddb/1.png",
+        "https://i.ibb.co/Z8MsfvQ/1.png",
+        "https://i.ibb.co/myRTRX7/1.png",
+        "https://i.ibb.co/sgxszkf/1.png",
+        "https://i.ibb.co/jkqdKhQ/1.png",
+        "https://i.ibb.co/kMpmyGN/1.png",
+        "https://i.ibb.co/4W66fw0/1.png",
+        "https://i.ibb.co/RN4FvdH/1.png",
+        "https://i.ibb.co/N3TKrNx/1.png",
+        "https://i.ibb.co/xGmGVrV/1.png",
+        "https://i.ibb.co/B6VWy5f/1.png",
+      ];
+
+      let paths: Slice[] = [];
+
+      for (let i = 0; i < _paths.length; i++) {
+        paths.push(new Slice(_paths[i], i as any as string));
+      }
+
+      return paths;
+    },
   },
 });
-</script>
 
+//TODO: SignalR coms
+//TODO:
+</script>
