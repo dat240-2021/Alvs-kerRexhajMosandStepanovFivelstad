cd9033df5ae32370f1c36b205414f4f61acedc5c
ardijanr <a.rexhaj@stud.uis.no>
2021-11-17 14:21:20 +0100

    proporional slicing, does not change image size anymore

--- website/backend/Core/Domain/Images/ImageSliceHelpers/CreateSlice.cs
+++ website/backend/Core/Domain/Images/ImageSliceHelpers/CreateSlice.cs
@@ -10,15 +10,14 @@ namespace backend.Core.Domain.Images.ImageSliceHelpers
         //Takes in an image as Rgba32 and start xPicel and yPixel starting point for the slice to create, plus the sliceSize.
         //Each pixel that is not part of the slice to keep is set to Rgba = 0. The returned image is of type png (returned as byte[]).
         //The returned image is of total size 525x525 pixels, which is the same size as the original imaged passed in. SixLabors.ImageSharp library is used for the image manipulation.
-        public static byte[] CreateOneSlice(Image<Rgba32> image, int xPixel, int yPixel,int sliceSize)
+        public static byte[] CreateOneSlice(Image<Rgba32> image, int xPixel, int yPixel,(int,int) sliceSize)
         {
             for (var x = 0; x < image.Width; x++)
             {
                 for (var y = 0; y < image.Height; y++)
                 {
-                    if ((x >= xPixel && x < (xPixel + sliceSize)) && (y >= yPixel && y < (yPixel+sliceSize)))
+                    if ((x >= xPixel && x < (xPixel + sliceSize.Item1)) && (y >= yPixel && y < (yPixel+sliceSize.Item2)))
                     {
-
                     }
                     else
                     {
