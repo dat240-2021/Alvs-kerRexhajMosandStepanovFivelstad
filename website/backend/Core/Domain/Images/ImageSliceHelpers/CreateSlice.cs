88a22059494769fa849367540f41b3817afaa17b
Fivel1974 <t.fivelstad@stud.uis.no>
2021-11-25 14:31:02 +0100

    Optimized image slicing time for both manual and automatic modes by resizing images over 1000px in height. Changed auto slicing to create a blank image and copying the slice pixels needed into this, instead of setting pixels to background color. Added some styling to index/login-register page.

--- website/backend/Core/Domain/Images/ImageSliceHelpers/CreateSlice.cs
+++ website/backend/Core/Domain/Images/ImageSliceHelpers/CreateSlice.cs
@@ -3,31 +3,28 @@ using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats.Png;
 using SixLabors.ImageSharp.PixelFormats;
 
+
 namespace backend.Core.Domain.Images.ImageSliceHelpers
 {
     public class CreateSlice
     {
         //Takes in an image as Rgba32 and start xPicel and yPixel starting point for the slice to create, plus the sliceSize.
-        //Each pixel that is not part of the slice to keep is set to Rgba = 0. The returned image is of type png (returned as byte[]).
-        //The returned image is of total size 525x525 pixels, which is the same size as the original imaged passed in. SixLabors.ImageSharp library is used for the image manipulation.
+        //Creates a blank image of the same size as the input image and copies the needed pixels from the original image to the new image.
+        //Return the resulting slice.w SixLabors.ImageSharp library is used for the image manipulation.
         public static byte[] CreateOneSlice(Image<Rgba32> image, int xPixel, int yPixel,(int,int) sliceSize)
         {
-            for (var x = 0; x < image.Width; x++)
-            {
-                for (var y = 0; y < image.Height; y++)
-                {
-                    if ((x >= xPixel && x < (xPixel + sliceSize.Item1)) && (y >= yPixel && y < (yPixel+sliceSize.Item2)))
-                    {
-                    }
-                    else
-                    {
-                        image[x, y] = Rgba32.ParseHex("#00000000");
-                    }
+            var resultImage = new Image<Rgba32>(image.Width,image.Height);
 
+            for (var x = xPixel; x < (xPixel + sliceSize.Item1); x++)
+            {
+                for (var y = yPixel; y < (yPixel + sliceSize.Item2); y++)
+                {
+                    resultImage[x, y] = image[x,y];
                 }
             }
+
             var ms = new MemoryStream();
-            image.Save(ms, PngFormat.Instance);
+            resultImage.Save(ms, PngFormat.Instance);
             return ms.ToArray();
         }
     }
